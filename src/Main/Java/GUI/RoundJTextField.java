ea4b3c08dc82c41345ebb722a2a35f3a5c9b4a79
Justin-Aye <balcera85@gmail.com>
2024-02-12 13:46:56 +0800

    Set Default Value of textfields to 0

--- src/Main/Java/GUI/RoundJTextField.java
+++ src/Main/Java/GUI/RoundJTextField.java
@@ -10,16 +10,20 @@ public class RoundJTextField extends JTextField {
         super(size);
         setOpaque(false); // As suggested by @AVD in comment.
         setHorizontalAlignment(JTextField.CENTER);
+        setText("0");
     }
+
     protected void paintComponent(Graphics g) {
         g.setColor(getBackground());
         g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
         super.paintComponent(g);
     }
+
     protected void paintBorder(Graphics g) {
         g.setColor(getForeground());
         g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 20, 20);
     }
+    
     public boolean contains(int x, int y) {
         if (shape == null || !shape.getBounds().equals(getBounds())) {
             shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 20, 20);
