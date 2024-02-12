2a4a87ae2506b0c8e752ffebc48967a23d3cf6af
cifelse <meetcif@gmail.com>
2024-02-12 20:41:28 +0800

    Separate Forms

--- src/Main/Java/GUI/Control_Panel.java
+++ src/Main/Java/GUI/Control_Panel.java
@@ -32,19 +32,48 @@ public class Control_Panel extends JPanel {
                 float s = Float.parseFloat(topPanel.speed.getText());
                 float a = Integer.parseInt(topPanel.angle.getText());
                 int c = Integer.parseInt(topPanel.count.getText());
+
+                // For Form 1
+                int x1 = Integer.parseInt(topPanel.x1.getText());
+                int y1 = Integer.parseInt(topPanel.y1.getText());
+                int x2 = Integer.parseInt(topPanel.x2.getText());
+                int y2 = Integer.parseInt(topPanel.y2.getText());
+                boolean isForm1 = x1 != 0 && y1 != 0 && x2 != 0 && y2 != 0;
+
+                // For Form 2
                 int theta_1 = Integer.parseInt(topPanel.theta_1.getText());
                 int theta_2 = Integer.parseInt(topPanel.theta_1.getText());
+                boolean isForm2 = theta_1 != 0 && theta_2 != 0;
 
-                if(theta_1 != 0 && theta_2 != 0){}
+                // For Form 3
+                int v1 = Integer.parseInt(topPanel.v1.getText());
+                int v2 = Integer.parseInt(topPanel.v2.getText());
+                boolean isForm3 = v1 != 0 && v2 != 0;
 
-                if(c > 0){
+                // Execute when Count is more than 0
+                if (c > 0) {
                     executor.execute(new Runnable() {
                         @Override
                         public void run() {
                             for(int i = 0; i < c; i++) {
                                 try {
                                     Thread.sleep(100);
-                                    sr.Add_Particle(new Particle(s, a + (i*10), 5));
+                                    
+                                    // First Form
+                                    if (isForm1) {
+                                        sr.Add_Particle(new Particle(s, a + (i*10), 5));
+                                    }
+
+                                    // Second Form
+                                    else if (isForm2) {
+                                        sr.Add_Particle(new Particle(s, a + (i*10), 5));
+                                    }
+
+                                    // Third Form
+                                    else if (isForm3) {
+                                        sr.Add_Particle(new Particle(s, a + (i*10), 5));
+                                    }
+
                                 } catch (Exception exception) {
                                     exception.printStackTrace();
                                 }
@@ -52,7 +81,6 @@ public class Control_Panel extends JPanel {
                         }
                     });
                 }
-                    
             }
         });
 
