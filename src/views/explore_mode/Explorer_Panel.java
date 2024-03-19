f2b969d0c24960679d9278d77a9b779775c8b001
Justin-Aye <balcera85@gmail.com>
2024-03-19 21:22:38 +0800

    Modified explorer window and panel

---
+++ src/views/explore_mode/Explorer_Panel.java
@@ -0,0 +1,165 @@
+package views.explore_mode;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import models.Explorer;
+import models.Particle;
+import models.Resources;
+import models.Wall;
+import views.dev_mode.SimPanel;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+
+public class Explorer_Panel extends JPanel implements ActionListener, KeyListener{
+
+    private Resources resources;
+    private ExecutorService executor;
+    private CopyOnWriteArrayList<Particle> particles;
+    private Explorer explorer;
+    private Timer timer;
+    private int FRAME_RATE = 15;
+    private int width_ratio, height_ratio;
+    private int center_x, center_y;
+
+    public Explorer_Panel(ExecutorService e, Resources r){
+        this.resources = r;
+        this.executor = e;
+        this.particles = resources.getParticles();
+
+        this.width_ratio = (int) Math.ceil(Explorer_Window.WIDTH / 33.0);
+        this.height_ratio = (int) Math.ceil(Explorer_Window.HEIGHT / 19);
+        this.center_x = Explorer_Window.WIDTH / 2;
+        this.center_y = Explorer_Window.HEIGHT / 2;
+
+        this.explorer = new Explorer(1200, 650, Explorer_Window.WIDTH, Explorer_Window.HEIGHT);
+
+        setBackground(Color.WHITE);
+        setPreferredSize(new Dimension(Explorer_Window.WIDTH, Explorer_Window.HEIGHT));
+        setMinimumSize(new Dimension(Explorer_Window.WIDTH, Explorer_Window.HEIGHT));
+
+        setFocusable(true);
+        addKeyListener(this);
+
+        timer = new Timer(FRAME_RATE, this);
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (g instanceof Graphics2D) {
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setColor(Color.RED);
+            g2d.fillRect(
+                this.center_x - width_ratio, 
+                this.center_y - height_ratio, 
+                width_ratio * 2 + 1, 
+                height_ratio * 2 + 1
+            );
+            
+            g2d.setColor(Color.BLACK);
+            synchronized(particles) {
+                for (Particle p : particles) {
+                    if(explorer.isDetected(p.getX(), p.getY()))
+                        g2d.fillOval(
+                            (p.getX() - explorer.getV1_x()) * width_ratio, 
+                            (p.getY() - explorer.getV1_y()) * height_ratio, 
+                            Particle.DIAMETER * height_ratio, 
+                            Particle.DIAMETER * height_ratio
+                        );
+                }
+            }
+
+            if(this.explorer.getCenter_x() - 16 < 0){
+                g2d.fillRect(0, 0, Math.abs(this.explorer.getCenter_x() - 16)*width_ratio, Explorer_Window.HEIGHT);
+            }
+
+            if(this.explorer.getCenter_x() + 16 > Explorer_Window.WIDTH){
+                int overflow = this.explorer.getCenter_x() + 16;
+                int width = (overflow - Explorer_Window.WIDTH) * width_ratio;
+                g2d.fillRect(Explorer_Window.WIDTH - (width), 0, width, Explorer_Window.HEIGHT);
+            }
+
+            if(this.explorer.getCenter_y() - 9 < 0){
+                g2d.fillRect(0, 0, Explorer_Window.WIDTH, Math.abs(this.explorer.getCenter_y() - 9)*height_ratio);
+            }
+
+            if(this.explorer.getCenter_x() + 9 > Explorer_Window.HEIGHT){
+                int overflow = this.explorer.getCenter_y() + 9;
+                int height = (overflow - Explorer_Window.HEIGHT) * height_ratio;
+                g2d.fillRect(0, Explorer_Window.HEIGHT - height, Explorer_Window.WIDTH, height);
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        char key = e.getKeyChar();
+        switch (key) {
+            case 'w':
+                this.explorer.move_up();
+                System.out.println(explorer.getCenter_x() + " " + explorer.getCenter_y());
+                break;
+
+            case 'a':
+                this.explorer.move_left();
+                System.out.println(explorer.getCenter_x() + " " + explorer.getCenter_y());
+                break;
+        
+            case 's':
+                this.explorer.move_down();
+                System.out.println(explorer.getCenter_x() + " " + explorer.getCenter_y());
+                break;
+
+            case 'd':
+                this.explorer.move_right();
+                System.out.println(explorer.getCenter_x() + " " + explorer.getCenter_y());
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for (Particle particle : particles) {
+            this.executor.submit(() -> {
+                synchronized(particle) {
+                    int dia = Particle.DIAMETER;
+
+                    // // Check if particle hits walls of the SimPanel
+                    if (particle.getX() - (dia / 2) <= 0 || particle.getX() + (dia / 2) >= (Explorer_Window.WIDTH)) {
+                        particle.setVelocityX(-particle.getVelocityX());
+                    }
+                    
+                    if (particle.getY() - (dia / 2) <= 0 || particle.getY() + (dia / 2) >= (Explorer_Window.HEIGHT)) {
+                        particle.setVelocityY(-particle.getVelocityY());
+                    }
+
+                    // Update the position of the particle
+                    particle.refreshXY();
+                }
+            });
+        }
+
+        // Repaint the SimPanel
+        repaint();
+    }
+}
---
+++ src/views/explore_mode/Explorer_Window.java
@@ -0,0 +1,63 @@
+package views.explore_mode;
+
+import java.util.concurrent.ExecutorService;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+import models.Resources;
+
+
+/**
+ * The Explorer Window class is a JFrame that is used to display the explorer's POV.
+ */
+public class Explorer_Window extends JFrame{
+    
+    // Title
+    public static final String TITLE = "Explorer";
+
+    // Window Size
+    public final static int WIDTH = 1280;
+    public final static int HEIGHT = 720;
+
+    public Explorer_Window(ExecutorService executor, Resources resources){
+        // Set the title of the window
+        super(TITLE);
+
+        // Set the default close operation
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Set the size of the window
+        setSize(new Dimension(WIDTH, HEIGHT));
+
+        // Set the window to be not resizable
+        setResizable(false);
+        
+        // Set the layout of the window
+        setLayout(new BorderLayout());
+
+        // Close everything when X is clicked
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                super.windowClosed(e);
+                executor.shutdownNow();
+                System.gc();
+            }
+        });
+
+        // Add the Main Panel to the window
+        Explorer_Panel panel = new Explorer_Panel(executor, resources);
+        getContentPane().add(panel, BorderLayout.CENTER);
+
+        // Set the window to be visible
+        pack();
+        setVisible(true);
+    }
+}
